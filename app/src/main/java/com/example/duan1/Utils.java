a41c53e9a7bec2cfb4520c0fb8ec09a3ef83eff1
Vu Ba Huan <bahuan305@gmail.com>
2023-11-17 22:35:19 +0700

    đăng ký, đổi mật khẩu

--- app/src/main/java/com/example/duan1/Utils.java
+++ app/src/main/java/com/example/duan1/Utils.java
@@ -4,4 +4,6 @@ public class Utils {
     public static final String CUSTOM = "custom";
     public static final String SHOP = "shop";
     public static final String TYPE = "type";
+    public static final String EMAIL = "email";
+    public static final String ROLE = "role";
 }
