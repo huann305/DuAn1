4ab60c3bfcf34387d6f923652eabbd2ba86b4789
Vu Ba Huan <bahuan305@gmail.com>
2023-11-17 17:43:07 +0700

    trang chu

--- app/src/main/java/com/example/duan1/user/fragment/home/HomeAdapter.java
+++ app/src/main/java/com/example/duan1/user/fragment/home/HomeAdapter.java
@@ -3,13 +3,24 @@ package com.example.duan1.user.fragment.home;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.duan1.R;
+import com.example.duan1.model.Product;
 
-public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
+import java.util.List;
+
+public abstract class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
+
+    List<Product> list;
+
+    public HomeAdapter(List<Product> list) {
+        this.list = list;
+    }
 
     @NonNull
     @Override
@@ -19,18 +30,40 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+        Product product = list.get(position);
 
+        if (product.getImage() != null) {
+//            holder.imgProduct.setImageBitmap(product.getImage());
+        }
+        holder.tvNameProduct.setText(product.getName());
+        holder.tvPrice.setText(product.getPrice() + " VND");
+        holder.tvQuantitySold.setText(product.getQuantitySold() + " đã bán");
+
+        holder.layout.setOnClickListener(v -> onItemClick(position));
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return list.size();
     }
 
+    public abstract void onItemClick(int position);
+
     public class HomeViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgProduct;
+        TextView tvNameProduct;
+        TextView tvPrice;
+        TextView tvQuantitySold;
+
+        View layout;
 
         public HomeViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgProduct = itemView.findViewById(R.id.imageView);
+            tvNameProduct = itemView.findViewById(R.id.textView3);
+            tvPrice = itemView.findViewById(R.id.textView5);
+            tvQuantitySold = itemView.findViewById(R.id.textView7);
+            layout = itemView.findViewById(R.id.layout_item);
         }
     }
 }
--- app/src/main/java/com/example/duan1/user/fragment/home/HomeFragment.java
+++ app/src/main/java/com/example/duan1/user/fragment/home/HomeFragment.java
@@ -1,12 +1,22 @@
 package com.example.duan1.user.fragment.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.duan1.R;
 import com.example.duan1.admin.ui.fragment.BaseFragment;
+import com.example.duan1.dao.CustomerDAO;
+import com.example.duan1.dao.ProductDAO;
 import com.example.duan1.databinding.FragmentHomeCustomerBinding;
+import com.example.duan1.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends BaseFragment<FragmentHomeCustomerBinding> {
     public static final String TAG = "Trang chủ";
@@ -23,18 +33,30 @@ public class HomeFragment extends BaseFragment<FragmentHomeCustomerBinding> {
         return R.layout.fragment_home_customer;
     }
 
+    List<Product> list;
+    ProductDAO productDAO;
     @Override
     protected void initView() {
-        binding.rcvHomePage1.setLayoutManager(new LinearLayoutManager(getActivity()));
-        binding.rcvHomePage2.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.rcvHomePage1.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 
-        binding.rcvHomePage1.setAdapter(new HomeAdapter());
-        binding.rcvHomePage2.setAdapter(new HomeAdapter());
+        binding.rcvHomePage1.setAdapter(new HomeAdapter(list) {
+            @Override
+            public void onItemClick(int position) {
+//                Intent intent = new Intent(getContext(), ProductDetailActivity.class);
+//                intent.putExtra("id", list.get(position).getId());
+//                startActivity(intent);
+                Toast.makeText(getContext(), position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     protected void initData() {
+        list = new ArrayList<>();
+        productDAO = new ProductDAO(getContext());
 
+        list = productDAO.getAll();
+        Log.d("TAGG", "initData: " + list.get(0).getName());
     }
 
     @Override
